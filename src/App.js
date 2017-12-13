fb69318cc3e09cd1b667cfffb1f6043e93ceccd8
Drew Hollenback <drewsee26@gmail.com>
2017-12-13 13:57:25 -0700

    updated Library component and added album data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom'
 import Landing from './components/Landing'
 import Library from './components/Library'
+import Album from './components/Album'
 import './App.css';
 
 class App extends Component {
@@ -12,12 +13,14 @@ class App extends Component {
               <nav>
                   <Link to='/'>Landing</Link>
                   <Link to='/library'>Library</Link>
+
               </nav>
               <h1>Bloc Jams</h1>
           </header>
           <main>
               <Route exact path="/" component={Landing} />
               <Route exact path="/library" component={Library} />
+              <Route path="/album/:slug" component={Album} />
           </main>
       </div>
     );
--- src/components/Album.js
+++ src/components/Album.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Album = () => (
-    <section className='album'>
-        This is where the album will go
-    </section>
-)
+class Album extends Component {
+    render() {
+        return (
+            <section className='album'>
+                {this.props.match.params.slug} Album will go here
+            </section>
+
+        )
+    }
+}
+
 
 export default Album
